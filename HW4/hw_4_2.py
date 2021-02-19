45e82f30311b8fbf27eb0d8d3b355df5a69dd3ae
Vladimir <chamorsev@mail.ru>
2021-02-20 00:40:15 +0300

    HomeWork4

---
+++ HW4/hw_4_2.py
@@ -0,0 +1,27 @@
+
+
+def currency_rates(charcode):
+    import requests
+
+    curses = {}
+
+    url = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text.split("</Valute>")
+
+
+    for i in url:
+        incode = i.find('<CharCode>') + len('<CharCode>')
+        outcode = i.find('</CharCode>')
+        invalue = i.find('<Value>') + len('<Value>')
+        outvalue = i.find('</Value>')
+        codekey = i[incode:outcode]
+        valuename = i[invalue:outvalue]
+        curses[codekey] = valuename.replace(',','.')
+    del curses['']
+    curse = curses.get(charcode.upper())
+
+    try:
+        print(float(curse))
+    except:
+        print(None)
+currency_rates('USD')
+currency_rates('EUR')
---
+++ HW4/hw_4_4.py
@@ -0,0 +1,2 @@
+import utils
+utils.currency_rates('USD')
