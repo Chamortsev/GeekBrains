7555a7f4451bc896ec0d767e17b2c7420e089b1e
Vladimir <chamorsev@mail.ru>
2021-03-03 10:16:32 +0300

    HW 5

--- HW5/hw_5_1.py
+++ HW5/hw_5_1.py
@@ -1,8 +1,10 @@
-a=20
+a = 20
+
 
 def nums_generator(max_num):
-   for num in range(1, max_num + 1, 2):
-       yield num
+    for num in range(1, max_num + 1, 2):
+        yield num
+
 
 nums_gen = nums_generator(a)
 try:
--- HW5/hw_5_4.py
+++ HW5/hw_5_4.py
@@ -1,29 +1,25 @@
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
 # Вариант решения 1
-tmp =src[0]
+tmp = src[0]
 result = []
 for i in src:
-        if (i) > tmp:
-             tmp=(i)
-             result.append(i)
-        tmp=(i)
+    if i > tmp:
+        tmp = i
+        result.append(i)
+    tmp = i
 print(result)
 
 # Вариант решения 2
-tmp =src[0]
+tmp = src[0]
 
 unique_gen = set()
-temp =set()
+temp = set()
 for i in src:
-    if (i) > tmp:
+    if i > tmp:
         unique_gen.add(i)
     else:
         unique_gen.discard(i)
     temp.add(i)
-    tmp = (i)
+    tmp = i
 print(unique_gen)
-
-
-
-
